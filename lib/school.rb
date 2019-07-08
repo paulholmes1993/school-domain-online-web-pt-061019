8ea4c6dcf83942c2bab67fad05b70325b597d758
Paul Holmes <paulholmes1993@gmail.com>
2019-07-08 17:30:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,11 @@ end
 def add_student(name, grade)
   @student_name = name
   @grade = grade
+  if @roster.include?(grade) == false
+    @roster[grade] = []
+  end
+  @roster[grade] << name
+end
 
 def grade(number)
   @roster[number]
